9b0ab904ebd93fec93ed2a2fd53cb5cbc088049e
Chris Cooney <chris.cooney05@hotmail.co.uk>
2020-06-03 17:41:03 +0100

    refactored feature_test_spec to use form to submit url

--- spec/features/feature_test_spec.rb
+++ spec/features/feature_test_spec.rb
@@ -29,19 +29,18 @@ feature 'Add bookmark' do
   end
 end
 
-ffeature 'Viewing bookmarks' do
+feature 'Viewing bookmarks' do
   scenario 'A user can see bookmarks' do
     connection = PG.connect(dbname: 'bookmark_manager_test')
 
-    connection.exec("INSERT INTO bookmarks VALUES (1, 'http://www.makersacademy.com');")
-    connection.exec("INSERT INTO bookmarks VALUES (2, 'http://www.destroyallsoftware.com');")
-    connection.exec("INSERT INTO bookmarks VALUES (3, 'http://www.google.com');")
+    visit '/addbookmark'
+    fill_in 'url', with: 'https://www.bbc.co.uk/'
+    click_button 'Submit'
+    visit '/addbookmark'
+    fill_in 'url', with: 'http://www.makersacademy.com'
+    click_button 'Submit'
 
-    visit('/')
-    click_button('Bookmarks')
-
+    expect(page).to have_content "https://www.bbc.co.uk/"
     expect(page).to have_content "http://www.makersacademy.com"
-    expect(page).to have_content "http://www.destroyallsoftware.com"
-    expect(page).to have_content "http://www.google.com"
   end
 end
