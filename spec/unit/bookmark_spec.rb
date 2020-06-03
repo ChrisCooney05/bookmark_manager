1dc9ad27777082e0132f1108b9903544ed0e06e8
Chris Cooney <chris.cooney05@hotmail.co.uk>
2020-06-03 08:50:44 +0100

    cleaned up test code + feature test runs through homepage

--- spec/unit/bookmark_spec.rb
+++ spec/unit/bookmark_spec.rb
@@ -10,10 +10,9 @@ describe Bookmark do
     it 'returns a list of bookmarks' do
       connection = PG.connect(dbname: 'bookmark_manager_test')
   
-      # Add the test data
-      connection.exec("INSERT INTO bookmarks (url) VALUES ('http://www.makersacademy.com');")
-      connection.exec("INSERT INTO bookmarks (url) VALUES('http://www.destroyallsoftware.com');")
-      connection.exec("INSERT INTO bookmarks (url) VALUES('http://www.google.com');")
+      connection.exec("INSERT INTO bookmarks VALUES (1, 'http://www.makersacademy.com');")
+      connection.exec("INSERT INTO bookmarks VALUES (2, 'http://www.destroyallsoftware.com');")
+      connection.exec("INSERT INTO bookmarks VALUES (3, 'http://www.google.com');")
   
       bookmarks = Bookmark.all
   
